dffcbc6636d485693c8f2c7d24d1b0c7e4258127
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2021-10-09 10:24:57 +0200

    initial

---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	type Person struct {
+		name string
+		Age  uint64
+	}
+
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	app.Get("/:name/:age", func(c *fiber.Ctx) error {
+		age, _ := strconv.ParseUint(c.Params("age"), 10, 8)
+		name := c.Params("name")
+		msg := Person{
+			name: name,
+			Age:  age,
+		}
+		return c.JSON(msg)
+	})
+
+	app.Listen(":3000")
+}
